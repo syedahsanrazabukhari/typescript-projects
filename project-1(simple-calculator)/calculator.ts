1fdbaf9a6f59337ecd5ce398bda2d87a02c85647
syedahsanrazabukhari <syedahsanrazabukhari10@gmail.com>
2024-03-21 19:17:35 +0500

    Simple calculator compiled

---
+++ project-1(simple-calculator)/calculator.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import inqurier from "inquirer";
+
+const answer = await inqurier.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Choose the operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+
+if (answer.operator === "addition") {
+    console.log(`${answer.firstnumber} + ${answer.secondnumber} = ${answer.firstnumber + answer.secondnumber}`);
+} else if (answer.operator === "subtraction") {
+    console.log(`${answer.firstnumber} - ${answer.secondnumber} = ${answer.firstnumber - answer.secondnumber}`);
+} else if (answer.operator === "multiplication") {
+    console.log(`${answer.firstnumber} x ${answer.secondnumber} = ${answer.firstnumber * answer.secondnumber}`);
+} else if (answer.operator === "division") {
+    console.log(`${answer.firstnumber} / ${answer.secondnumber} = ${answer.firstnumber / answer.secondnumber}`);
+} else {
+    console.error("number is not defined")
+}
+
+
+
+
